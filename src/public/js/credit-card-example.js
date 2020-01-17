3ea4d651b4d9a86c2a7a5830c09c679d2bc89d86
Stepan Lopushanskyi <stepanlopushanskyi@Stepans-MacBook-Pro.local>
2020-01-17 16:57:13 +0200

    add vgs collect env

--- src/public/js/credit-card-example.js
+++ src/public/js/credit-card-example.js
@@ -1,4 +1,4 @@
-var f = VGSCollect.create('{{VAULT_ID}}', function (state) {
+var f = VGSCollect.create('{{VAULT_ID}}', 'sandbox', function (state) {
 });
 
 var field = f.field('#cc-name .form-control', {
